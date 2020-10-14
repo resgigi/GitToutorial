723220db82effcb1e71ed77c476b932b1f0bba5d
Nguyen Xuan Quang <67813664+resgigi@users.noreply.github.com>
2020-10-14 14:04:39 +0700

    Update ViewController.swift

--- GitToutorial/ViewController.swift
+++ GitToutorial/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         print("lalala")
         print("lele")
+        print("code ga vai lon")
     }
 
 
